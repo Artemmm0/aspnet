b3e395455ee4a81b3e906bee7aa0e5e9885d138c
ArtemRus <95334625+Artemmm0@users.noreply.github.com>
2022-12-25 23:56:28 +0300

    Выполнить лр 2

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -27,10 +27,12 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.ConfigureRepositoryManager();
+            services.AddControllers();
             services.ConfigureCors();
             services.ConfigureIISIntegration();
             services.ConfigureLoggerService();
+            services.ConfigureSqlContext(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
