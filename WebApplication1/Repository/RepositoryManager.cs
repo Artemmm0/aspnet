fd630e2d68ff2c01a181caebf48a548ab0dc8fc7
ArtemRus <95334625+Artemmm0@users.noreply.github.com>
2022-12-25 23:58:01 +0300

    Добавить модели

--- WebApplication1/Repository/RepositoryManager.cs
+++ WebApplication1/Repository/RepositoryManager.cs
@@ -11,6 +11,8 @@ namespace Repository
         private RepositoryContext _repositoryContext;
         private ICompanyRepository _companyRepository;
         private IEmployeeRepository _employeeRepository;
+        private IProductRepository _productRepository;
+        private ICustomerRepository _customerRepository;
 
         public RepositoryManager(RepositoryContext repositoryContext)
         {
@@ -34,6 +36,25 @@ namespace Repository
                 if (_employeeRepository == null)
                     _employeeRepository = new EmployeeRepository(_repositoryContext);
                 return _employeeRepository;
+            }
+        }
+
+        public IProductRepository Product
+        {
+            get
+            {
+                if (_productRepository == null) 
+                    _productRepository = new ProductRepository(_repositoryContext);
+                return _productRepository;
+            }
+        }
+        public ICustomerRepository Customer
+        {
+            get
+            {
+                if (_customerRepository == null)
+                    _customerRepository = new CustomerRepository(_repositoryContext);
+                return _customerRepository;
             }
         }
 
