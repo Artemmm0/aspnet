3b0526409310513f49364f108ff5ecd376fef96e
ArtemRus <95334625+Artemmm0@users.noreply.github.com>
2022-12-26 01:34:00 +0300

    Выполнить лр4

--- WebApplication1/Contracts/ICustomerRepository.cs
+++ WebApplication1/Contracts/ICustomerRepository.cs
@@ -7,6 +7,7 @@ namespace Contracts
 {
     public interface ICustomerRepository
     {
-        IEnumerable<Customer> GetAllCustomers(bool trackChanges);
+        IEnumerable<Customer> GetAllCustomers(Guid productId, bool trackChanges);
+        Customer GetCustomer(Guid productId, Guid customerId, bool trackChanges);
     }
 }
--- WebApplication1/WebApplication1/Controller/CustomersController.cs
+++ WebApplication1/WebApplication1/Controller/CustomersController.cs
@@ -3,11 +3,12 @@ using Contracts;
 using Entities.DataTransferObjects;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 
 namespace WebApplication1.Controller
 {
-    [Route("api/[controller]")]
+    [Route("api/products/{productId}/customers")]
     [ApiController]
     public class CustomersController : ControllerBase
     {
@@ -23,13 +24,41 @@ namespace WebApplication1.Controller
         }
 
         [HttpGet]
-        public IActionResult GetCustomers()
+        public IActionResult GetCustomersForProduct(Guid productId)
         {
-            var customers = _repository.Customer.GetAllCustomers(trackChanges: false);
+            var product = _repository.Product.GetProduct(productId, false);
+            if (product == null)
+            {
+                _logger.LogInfo($"Product with id: {productId} doesn't exist in the database.");
+                return NotFound();
+            }
 
+            var customers = _repository.Customer.GetAllCustomers(productId, trackChanges: false);
             var customersDto = _mapper.Map<IEnumerable<CustomerDto>>(customers);
 
             return Ok(customersDto);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCustomerForProduct(Guid productId, Guid id)
+        {
+            var product = _repository.Product.GetProduct(productId, false);
+            if (product == null)
+            {
+                _logger.LogInfo($"Product with id: {productId} doesn't exist in the database.");
+                return NotFound();
+            }
+
+            var customer = _repository.Customer.GetCustomer(productId, id, trackChanges: false);
+            if (customer == null)
+            {
+                _logger.LogInfo($"Customer with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+
+            var customerDto = _mapper.Map<CustomerDto>(customer);
+
+            return Ok(customerDto);
         }
     }
 }
